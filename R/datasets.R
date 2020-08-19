f95f2b1a0aaeeb9f8480539b11b989dcf54a2d24
Mehmood Asghar <mah0001@gmail.com>
2020-08-18 20:27:55 -0400

    Add method for creating new documents

--- R/datasets.R
+++ R/datasets.R
@@ -165,7 +165,6 @@ import_ddi <- function(api_key=NULL,
 #'
 #'
 #'
-#' @param metadata Metadata array
 #' @export
 create_survey <- function(api_key=NULL,
                       api_base_url=NULL,
@@ -267,7 +266,7 @@ create_survey <- function(api_key=NULL,
 #'   )
 #' )
 #'
-#' createSurvey (
+#' create (
 #'   idno="survey-idno-test",
 #'   type="survey",
 #'   published = 1,
@@ -311,16 +310,375 @@ create <- function(api_key=NULL,
   options= c(options,metadata)
 
   url=get_api_url(paste0('datasets/create/',type,'/',idno))
-  httpResponse <- POST(url, add_headers("X-API-KEY" = api_key), body=metadata, content_type_json(), encode="json", accept_json(), verbose(get_verbose()))
+  httpResponse <- POST(url, add_headers("X-API-KEY" = api_key), body=options, content_type_json(), encode="json", accept_json(), verbose(get_verbose()))
 
   output=NULL
 
   if(httpResponse$status_code!=200){
     warning(content(httpResponse, "text"))
-  }else{
-    output=fromJSON(content(httpResponse,"text"))
   }
+
+
+  output=list(
+    "status_code"=httpResponse$status_code,
+    "response"=fromJSON(content(httpResponse,"text"))
+  )
 
   return (output)
 }
 
+
+
+
+
+#' Create new geospatial study
+#'
+#' Create a new geospatial study
+#'
+#' @return NULL
+#' @param idno (required) Study unique identifier
+#' @param repositoryid Collection ID that owns the study
+#' @param access_policy Select the access policy suitable for your data. Valid values - "open" "direct" "public" "licensed" "enclave" "remote" "other"
+#' @param data_remote_url Link to the website where the data is available. Required if access_policy is set to 'remote'.
+#' @param published Set status for study - 0 = Draft, 1 = Published
+#' @param overwrite Overwrite if a study with the same ID already exists? Valid values "yes", "no"
+#' @param metadata \strong{(required)} Metadata list depending on the type of study
+#'
+#' @examples
+#'
+#' metadata= list(
+#' metadata_maintenance=list(
+#' "update_frequency"="daily",
+#' "note"="Maintenane note",
+#' "contact"=list(
+#' list(
+#'     "person_name"="name here",
+#'     "role"="role"
+#'   )
+#' )
+#' )
+#'
+#' dataset_description=list(
+#' "file_identifier"="Unique IDNO",
+#' "language"="en",
+#' "contact"= list(
+#'     "person_name"= "name",
+#'     "role"= "string",
+#'   ),
+#'   "identification_info"=list(
+#'      "title"="title goes here",
+#'   )
+#' )
+#' )
+#'
+#' create_geospatial (
+#'   idno="survey-idno-test",
+#'   published = 1,
+#'   overwrite = "yes",
+#'   metadata_maintenance = list(),
+#'   dataset_description = list()
+#'   additional = list()
+#' )
+#'
+#'
+#'
+#'
+#' @export
+create_geospatial <- function(api_key=NULL,
+                   api_base_url=NULL,
+                   idno,
+                   repositoryid=NULL,
+                   access_policy=NULL,
+                   data_remote_url=NULL,
+                   published=NULL,
+                   overwrite=NULL,
+                   metadata=NULL){
+
+  if(is.null(api_key)){
+    api_key=get_api_key();
+  }
+
+  result = create(type="geospatial",
+                  idno=idno,
+                  repositoryid=repositoryid,
+                  access_policy=access_policy,
+                  data_remote_url = data_remote_url,
+                  published = published,
+                  overwrite= overwrite,
+                  metadata=metadata
+                  )
+
+  return (result)
+}
+
+
+
+
+
+
+#' Create new document
+#'
+#' Create a new document
+#'
+#' @return NULL
+#' @param idno (required) Study unique identifier
+#' @param repositoryid Collection ID that owns the study
+#' @param access_policy Select the access policy suitable for your data. Valid values - "open" "direct" "public" "licensed" "enclave" "remote" "other"
+#' @param data_remote_url Link to the website where the data is available. Required if access_policy is set to 'remote'.
+#' @param published Set status for study - 0 = Draft, 1 = Published
+#' @param overwrite Overwrite if a study with the same ID already exists? Valid values "yes", "no"
+#' @param metadata \strong{(required)} Metadata list
+#'
+#' @examples
+#'
+#' metadata=list(
+#' "repositoryid"= NULL,
+#' "published"= 1,
+#' "overwrite"= "no",
+#' "metadata_information"= list(
+#' "title"= "string",
+#' "idno"= "string",
+#' "producers"= list(
+#'  list(
+#'       "name"= "string",
+#'       "abbr"= "string",
+#'       "affiliation"= "string",
+#'       "role"= "string"
+#'     )
+#'   ),
+#'   "production_date"= "string",
+#'   "version"= "string"
+#' ),
+#' "document_description"= list(
+#'   "title_statement"= list(
+#'     "idno"= "document-unique-id",
+#'     "title"= "document title",
+#'     "sub_title"= "string",
+#'     "alternate_title"= "string",
+#'     "abbreviated_title"= "string"
+#'   ),
+#'   "type"= "article",
+#'   "description"= "string",
+#'   "toc"= "string",
+#'   "toc_structured"= list(
+#'     list(
+#'       "id"= "string",
+#'       "parent_id"= "string",
+#'       "name"= "string"
+#'     )
+#'   ),
+#'   "abstract"= "string",
+#'   "notes"= list(
+#'     list(
+#'       "note"= "string"
+#'     )
+#'   ),
+#'   "scope"= "string",
+#'   "ref_country"= list(
+#'     list(
+#'       "name"= "country name",
+#'       "code"= "string"
+#'     ),
+#'     list(
+#'       "name"= "country name 2",
+#'       "code"= "string"
+#'     )
+#'   ),
+#'   "spatial_coverage"= "string",
+#'   "temporal_coverage"= "string",
+#'   "date_created"= "string",
+#'   "date_available"= "string",
+#'   "date_modified"= "string",
+#'   "date_published"= "string",
+#'   "id_numbers"= list(
+#'     "type"= "string",
+#'     "value"= "string"
+#'   ),
+#'   "publication_frequency"= "string",
+#'   "languages"= list(
+#'     list(
+#'       "name"= "string",
+#'       "code"= "string"
+#'     )
+#'   ),
+#'   "license"= list(
+#'     list(
+#'       "name"= "string",
+#'       "uri"= "string"
+#'     )
+#' ),
+#'   "bibliographic_citation"= "string",
+#'   "chapter"= "string",
+#'   "edition"= "string",
+#'   "institution"= "string",
+#'   "journal"= "string",
+#'   "volume"= "string",
+#'   "issue"= "string",
+#'   "pages"= "string",
+#'   "series"= "string",
+#'   "creator"= "string",
+#'   "authors"= list(
+#'     list(
+#'       "first_name"= "string",
+#'       "initial"= "string",
+#'       "last_name"= "string",
+#'       "affiliation"= "string"
+#'     )
+#'   ),
+#'   "editors"= list(
+#'     list(
+#'       "first_name"= "string",
+#'       "initial"= "string",
+#'       "last_name"= "string",
+#'       "affiliation"= "string"
+#'     )
+#'   ),
+#'   "translators"= list(
+#'     list(
+#'       "first_name"= "string",
+#'       "initial"= "string",
+#'       "last_name"= "string",
+#'       "affiliation"= "string"
+#'     )
+#'   ),
+#'   "contributors"= list(
+#'     list(
+#'       "first_name"= "string",
+#'       "initial"= "string",
+#'       "last_name"= "string",
+#'       "affiliation"= "string"
+#'     )
+#'   ),
+#'   "publisher"= "string",
+#'   "publisher_address"= "string",
+#'   "rights"= "string",
+#'   "copyright"= "string",
+#'   "usage_terms"= "string",
+#'   "security_classification"= "string",
+#'   "access_restrictions"= "string",
+#'   "sources"= list(
+#'     "data_source"= list(),
+#'     "source_origin"= "string",
+#'     "source_char"= "string",
+#'     "source_doc"= "string"
+#'   ),
+#'   "keywords"= list(
+#'     list(
+#'       "name"= "string",
+#'       "vocabulary"= "string",
+#'       "uri"= "string"
+#'     )
+#'   ),
+#'   "themes"= list(
+#'     list(
+#'       "name"= "string",
+#'       "vocabulary"= "string",
+#'       "uri"= "string"
+#'     )
+#'   ),
+#'   "topics"= list(
+#'     list(
+#'       "id"= "string",
+#'       "name"= "string",
+#'       "parent_id"= "string",
+#'       "vocabulary"= "string",
+#'       "uri"= "string"
+#'     )
+#'   ),
+#'   "disciplines"= list(
+#'     list(
+#'       "name"= "string",
+#'       "vocabulary"= "string",
+#'       "uri"= "string"
+#'     )
+#'   ),
+#'   "audience"= "string",
+#'   "mandate"= "string",
+#'   "pricing"= "string",
+#'   "relations"= list(
+#'     list(
+#'       "name"= "string",
+#'       "type"= "isPartOf"
+#'     )
+#'   ),
+#'   "lda_topics"= list(
+#'     list(
+#'       "model_info"= list(
+#'         list(
+#'           "source"= "string",
+#'           "author"= "string",
+#'           "version"= "string",
+#'           "model_id"= "string",
+#'           "nb_topics"= 0,
+#'           "description"= "string",
+#'           "corpus"= "string",
+#'           "uri"= "string"
+#'         )
+#'       ),
+#'       "topic_description"= list(
+#'         list(
+#'           "topic_id"= 1,
+#'           "topic_score"= .01,
+#'           "topic_label"= "label",
+#'           "topic_words"= list(
+#'             list(
+#'               "word"= "string"
+#'             )
+#'           )
+#'         )
+#'       )
+#'     )
+#'   )
+#' ),
+#' "tags"= list(
+#'   list(
+#'     "tag"= "string"
+#'   )
+#' ),
+#' "files"= list(
+#'   list(
+#'     "file_uri"= "string",
+#'     "format"= "string",
+#'     "location"= "string",
+#'     "note"= "string"
+#'   )
+#' )
+#' )
+#'
+#' create_document (
+#'   idno="document-idno",
+#'   published = 1,
+#'   overwrite = "yes",
+#'   metadata = metadata
+#' )
+#'
+#'
+#'
+#'
+#' @export
+create_document <- function(api_key=NULL,
+                              api_base_url=NULL,
+                              idno,
+                              repositoryid=NULL,
+                              access_policy=NULL,
+                              data_remote_url=NULL,
+                              published=NULL,
+                              overwrite=NULL,
+                              metadata=NULL){
+
+  if(is.null(api_key)){
+    api_key=get_api_key();
+  }
+
+  result = create(type="document",
+                  idno=idno,
+                  repositoryid=repositoryid,
+                  access_policy=access_policy,
+                  data_remote_url = data_remote_url,
+                  published = published,
+                  overwrite= overwrite,
+                  metadata=metadata
+  )
+
+  return (result)
+}
