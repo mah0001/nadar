f8cb15011be0d47b6ae609a094d3641a81bf2a1b
Mehmood Asghar <mah0001@gmail.com>
2021-05-05 12:11:17 -0400

    Add functions to delete widgets

--- R/widgets.R
+++ R/widgets.R
@@ -126,3 +126,96 @@ widgets_attach <- function(
 
   return (output)
 }
+
+
+#' Detach widget from a study
+#'
+#' Detach widgetfrom study
+#'
+#' @return NULL
+#' @param idno (required) Study IDNo
+#' @param uuid (required) Widget ID
+#'
+#' @export
+widgets_detach <- function(
+  idno,
+  uuid,
+  api_key=NULL,
+  api_base_url=NULL){
+
+  if(is.null(api_key)){
+    api_key=get_api_key();
+  }
+
+  options=list(
+    "idno"=idno,
+    "uuid"=uuid
+  )
+
+  url=get_api_url(paste0('widgets/detach_study'))
+  httpResponse <- POST(url,
+                       add_headers("X-API-KEY" = api_key),
+                       body=options,
+                       content_type_json(),
+                       encode="json",
+                       accept_json(),
+                       verbose(get_verbose()))
+
+  output=NULL
+
+  if(httpResponse$status_code!=200){
+    warning(content(httpResponse, "text"))
+  }
+
+
+  output=list(
+    "status_code"=httpResponse$status_code,
+    "response"= nada_http_response_json(httpResponse)
+  )
+
+  return (output)
+}
+
+
+
+
+
+#' Delete widget
+#'
+#' Delete widget
+#'
+#' @return NULL
+#' @param uuid (required) Widget ID
+#'
+#' @export
+widgets_delete <- function(
+  uuid,
+  api_key=NULL,
+  api_base_url=NULL){
+
+  if(is.null(api_key)){
+    api_key=get_api_key();
+  }
+
+  url=get_api_url(paste0('widgets/',uuid))
+  httpResponse <- DELETE(url,
+                       add_headers("X-API-KEY" = api_key),
+                       content_type_json(),
+                       encode="json",
+                       accept_json(),
+                       verbose(get_verbose()))
+
+  output=NULL
+
+  if(httpResponse$status_code!=200){
+    warning(content(httpResponse, "text"))
+  }
+
+
+  output=list(
+    "status_code"=httpResponse$status_code,
+    "response"= nada_http_response_json(httpResponse)
+  )
+
+  return (output)
+}
