0668635c5e16ffe8829be3c1c90e474030df8219
Mehmood Asghar <mah0001@gmail.com>
2022-05-12 15:55:46 -0400

    Add function catalog_search

--- R/catalog_entries.R
+++ R/catalog_entries.R
@@ -70,11 +70,152 @@ catalog_delete <- function(idno, api_key=NULL, api_base_url=NULL){
 #'
 #' Search catalog
 #'
+#' @param options - Search options:
+#' \itemize{
+#'   \item sk - Keywords
+#'   \item from - Year from
+#'   \item to | Year to
+#'   \item country | Country names or codes. To search for multiple countries, use pipe (|) to separate them e.g. Albania|Turkey
+#'   \item collection - Filter by one or more collections. e.g. `collection1, collection2`
+#'   \item created - Filter by date of creation. Use date format YYYY-MM-DD. For example, `2020/04/01` returns rows created on and after the date. To specify a date range, use 2020/04/01-2020/04/15
+#'   \item dtype - Data access types (`open`, `direct`, `public`, `licensed`, `remote`). For multiple values, use comma e.g. `open,direct`
+#' }
+#' @param page Page
+#' @param ps Page size (number of rows per page)
+#' @param sort_by Sort results by field, support fields are `title`,`year`,`nation`
+#' @param sort_order Sort results order, valid options are `asc`, `desc`
+#'
+#' @examples
+#'
+#' #example - keywords search
+#'
+#' catalog_search (
+#'   options=list(
+#'     sk="health survey"
+#'   ),
+#'   ps=1000
+#' )
+#'
+#' #example - keywords search + filter by country and collection
+#'
+#' catalog_search (
+#'   options=list(
+#'     sk="health survey",
+#'     country="albania|afghanistan",
+#'     collection="dhs"
+#'   ),
+#'   ps=1000
+#' )
+#'
+#'
+#'
+#'
 #' @return list
 #'
 #' @export
-catalog_search <- function(options=list()){
-  return ("TODO")
+catalog_search <- function(
+                     options=list(
+                       sk=NULL,
+                       from=NULL,
+                       to=NULL,
+                       country=NULL,
+                       collection=NULL,
+                       created=NULL,
+                       dtype=NULL
+                     ),
+                     page=0,
+                     ps=50,
+                     sort_by=NULL,
+                     sort_order=NULL,
+                     api_key=NULL,
+                     api_base_url=NULL){
+
+  if(is.null(api_key)){
+    api_key=get_api_key();
+  }
+
+  params<-c()
+  for (param in names(options)) {
+    if (!is.null(options[[param]])){
+      params<-append(params,paste0(param,'=',URLencode(options[[param]],reserved = TRUE)))
+    }
+  }
+
+  params=paste(params, collapse="&");
+
+  print(params)
+
+  if(ps <= 100){ # up to 500 one API call
+    endpoint <- paste0("catalog", "?page=", page, "&ps=", ps, '&',params)
+  }else { # if more than 500 requested, multiple API calls
+    endpoint=paste0("catalog", "?page=", page, "&ps=100", '&',params)
+  }
+
+  # Create url
+  if(is.null(api_base_url)){
+    url <- get_api_url(endpoint = endpoint)
+  } else {
+    url <- paste0(api_base_url,"/",endpoint)
+  }
+
+  httpResponse <- GET(url, add_headers("X-API-KEY" = api_key), accept_json(), verbose(get_verbose()))
+  output <- NULL
+
+  if(httpResponse$status_code!=200){
+    warning(content(httpResponse, "text"))
+    stop(content(httpResponse, "text"), call. = FALSE)
+  }
+
+  output <- fromJSON(content(httpResponse,"text"))
+
+  # add more API calls if limit > 500
+  if(ps > 100){
+    cur_datasets <- output$result$rows # adding result datasets for each call
+    num_entries_to_add <- min(ps, as.integer(output$result$found)) - 100 # number of entries to add (max of limit and available entries)
+
+    df_column_names <- colnames(cur_datasets)
+
+    if (page==0){
+      page=1
+    }
+
+    while(output$result$found > 0 & num_entries_to_add > 0){ # while more entires to add
+      page <- page + 1 # update offset
+      endpoint <- paste0("catalog", "?page=", page, "&ps=100",'&',params)
+
+      # Create URL
+      if(is.null(api_base_url)){
+        url <- get_api_url(endpoint = endpoint)
+      } else {
+        url <- paste0(api_base_url,"/",endpoint)
+      }
+
+      # API call
+      httpResponse <- GET(url, add_headers("X-API-KEY" = api_key), accept_json(), verbose(get_verbose()))
+
+      if(httpResponse$status_code!=200){
+        warning(content(httpResponse, "text"))
+        stop(content(httpResponse, "text"), call. = FALSE)
+      }
+
+      output <- fromJSON(content(httpResponse,"text"))
+
+      output_ds <- subset(output$result$rows, select = df_column_names)
+      cur_datasets <- rbind(cur_datasets, output_ds) # combine results
+      num_entries_to_add <- num_entries_to_add - 100 # update number of entries to add
+    }
+
+    output$datasets <- cur_datasets
+  }
+
+  structure(
+    list(
+      content = output,
+      api_url = url,
+      status_code = httpResponse$status_code
+    ),
+    class = "nada_catalog_search"
+  )
 }
 
 
